21e8da89d0520bcb2e3438adf50484f2ebf726f8
Danny Koppenhagen <mail@d-koppenhagen.de>
2017-06-14 17:55:02 +0200

    adds comment with link to the RFC source

--- src/interfaces/jrd.ts
+++ src/interfaces/jrd.ts
@@ -1,3 +1,10 @@
+/**
+ * This interfaces are following the specification from RFC-7033
+ * See: https://tools.ietf.org/html/rfc7033#section-4.4
+ * In addition it specifies also the `error`-Object key which will
+ * may be sent by some servers when an error occured.
+ */
+
 export interface Ijrd {
   properties?: {
     [link: string] : string | null
